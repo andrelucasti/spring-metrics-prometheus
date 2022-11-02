a87582f2399cdf2fbabf682fda48112ef694b371
Andre Lucas <andrelucastic@gmail.com>
2022-11-02 01:50:21 +0000

    Create project and first configuration

---
+++ src/test/java/com/andrelucas/medium/SpringbootPrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrelucas.medium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPrometheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
